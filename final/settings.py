ec90d3507d88135d56d4ea4cbfe01ed8310470ec
dongyun <hoodiesparrow@gmail.com>
2021-05-27 15:02:23 +0900

    cors localhost

--- final/settings.py
+++ final/settings.py
@@ -146,6 +146,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     # config('CORS_VUE_URL'),
     config('CORS_NETLIFY_URL'),
+    'http://localhost:8080',
 ]
 
 
